1fcda044522cdbb0955c95153acb2c0f55704646
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2023-03-02 21:16:22 +0100

    bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import Logo from "../Assets/Logo.png";
 import { BsCart2 } from "react-icons/bs";
@@ -15,56 +16,56 @@ import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
-
-
-
-  
-  const Navbar = () => {
-    const [openMenu, setOpenMenu] = useState(false);
-    const menuOptions = [
-        {
-          text: "Home",
-          icon: <HomeIcon />,
-        },
-        {
-          text: "About",
-          icon: <InfoIcon />,
-        },
-        {
-          text: "Testimonials",
-          icon: <CommentRoundedIcon />,
-        },
-        {
-          text: "Contact",
-          icon: <PhoneRoundedIcon />,
-        },
-        {
-          text: "Cart",
-          icon: <ShoppingCartRoundedIcon />,
-        },
-      ];
-
-      return (
-        <nav>
-          <div className="nav-logo-container">
-            <img src={Logo} alt="" />
-          </div>
-          <div className="navbar-links-container">
-            <a href="">Home</a>
-            <a href="">About</a>
-            <a href="">Testimonials</a>
-            <a href="">Contact</a>
-            <a href="">
-              <BsCart2 className="navbar-cart-icon" />
-            </a>
-            <button className="primary-button">Bookings Now</button>
-          </div>
-          <div className="navbar-menu-container">
-          <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-            </div>
-            <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container" >
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonials</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon"/>
+        </a>
+        <button className="primary-button">Bookings Now</button>
+        <HiOutlineBars3
+          className="navbar-menu-icon"
+          onClick={() => setOpenMenu(true)}
+        />
+      </div>
+      <Drawer
+        open={openMenu}
+        onClose={() => setOpenMenu(false)}
+        anchor="right"
+      >
         <Box
-          sx={{ width: 250 }}
+          sx={{ width: 300 }}
           role="presentation"
           onClick={() => setOpenMenu(false)}
           onKeyDown={() => setOpenMenu(false)}
@@ -72,7 +73,11 @@ import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
           <List>
             {menuOptions.map((item) => (
               <ListItem key={item.text} disablePadding>
-                <ListItemButton>
+                <ListItemButton
+                  component="a"
+                  href={`/${item.text.toLowerCase()}`}
+                  onClick={() => setOpenMenu(false)}
+                >
                   <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.text} />
                 </ListItemButton>
@@ -83,7 +88,6 @@ import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
         </Box>
       </Drawer>
     </nav>
-      );
+  );
 };
-
 export default Navbar;
